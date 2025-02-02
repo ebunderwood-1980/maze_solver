f737fc0da9bccc5a8db3e2879d6ae16fdb3bf3fc
Eric Underwood <e.b.underwood@gmail.com>
2025-02-02 15:07:49 -0800

    Added Point and Line classes

--- src/graphics.py
+++ src/graphics.py
@@ -4,8 +4,8 @@ class Window():
     def __init__(self, w, h):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__cnvs = Canvas(self.__root, height = h, width = w, bg='white')
-        self.__cnvs.pack(fill=BOTH, expand=1)
+        self.__canvas = Canvas(self.__root, height = h, width = w, bg='white')
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False 
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,6 +20,27 @@ class Window():
         print("Window Closed")
 
     def close(self): 
-        self.__running = False
+        self.__running = False 
+
+    def draw_line(self, line, fill_color='black'):
+        line.draw(self.__canvas, fill_color)
+ 
+
+class Point(): 
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y 
 
 
+class Line():
+    def __init__(self, beginning_point, end_point):
+        self.p1 = beginning_point
+        self.p2 = end_point
+
+    def draw(self, canvas, color='black'): 
+        canvas.create_line(self.p1.x, 
+                           self.p1.y, 
+                           self.p2.x, 
+                           self.p2.y, 
+                           fill=color, width=2)
+        
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from graphics import Window 
+from graphics import Window, Line, Point
 
 
 
 
 def main(): 
-    win = Window(800, 600) 
+    win = Window(800, 600)
+    p1 = Point(100,100)
+    p2 = Point(200, 200)
+    p3 = Point(300,100)
+    p4 = Point(600, 400)
+    my_Line = Line(p1, p2)
+    my_Line2 = Line(p3, p4)
+    win.draw_line(my_Line, 'black')
+    win.draw_line(my_Line2, 'red')
     win.wait_for_close()  
 
 
